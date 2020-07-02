25294b3b0317a4f025f33331fa753f48817e137a
Declan Murphy <declxn@gmail.com>
2020-07-01 19:57:52 -0500

    Updated the app, still not working but hey, it's an update.

--- app/controllers/api/names_controller.rb
+++ app/controllers/api/names_controller.rb
@@ -1,2 +1,11 @@
 class Api::NamesController < ApplicationController
+
+  def upcase
+    render 'query.json.jb'
+    @first_name = params['first_name']
+    @last_name = params['last_name']
+  end
+
+
 end
+
